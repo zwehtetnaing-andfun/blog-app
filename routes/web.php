957c486fe5479f821e4f8117e865fb9966eb76be
Zwe Htet <zwehtetnaing@andfun.biz>
2025-03-26 08:31:07 +0630

    setup ui

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 });
 
 Route::resource('/articles', ArticleController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
